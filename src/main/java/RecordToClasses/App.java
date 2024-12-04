668053a6e98f7bdd1752aa11b3b2118498c670f3
joy <joyceline.marealle@peopleshores.com>
2024-12-03 19:52:11 -0500

    Record to classes: records

---
+++ src/main/java/RecordToClasses/App.java
@@ -0,0 +1,10 @@
+package RecordToClasses;
+
+public class App {
+    public static void main (String [] args){
+        Product product1= new Product ("tissue",2.42, 1);
+
+        System.out.println(product1.name());
+    }
+}
+
---
+++ src/main/java/RecordToClasses/Product.java
@@ -0,0 +1,5 @@
+package RecordToClasses;
+
+public record Product(String name, double price , double quantity) {
+}
+
